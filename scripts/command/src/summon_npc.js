a3fc97158680acf38cc0e360168dfb68d3e98ccc
Nebrian Cent <nebriancent@gmail.com>
2025-10-13 11:03:21 -0700

    Update summon_npc.js OwnerOnly

--- scripts/command/src/summon_npc.js
+++ scripts/command/src/summon_npc.js
@@ -3,6 +3,7 @@ import { newCommand } from '../handle';
 newCommand({
     name:"summon_npc",
     description:"Summons a NPC at your location",
+    ownerOnly: true,
     run: (data) => {
         data.player.runCommand("function admin_cmds/summon_npc");
     }
