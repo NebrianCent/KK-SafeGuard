df08f9bc5254d145c487d08fc3d6bd7e162ce13b
Nebrian Cent <nebriancent@gmail.com>
2025-10-14 19:21:40 -0700

    Update copyinv.js

--- scripts/command/src/copyinv.js
+++ scripts/command/src/copyinv.js
@@ -11,10 +11,7 @@ newCommand({
         const setName = args.slice(1).join(" ").replace(/["@]/g, "");
         const targetPlayer = getPlayerByName(setName);
         if(!targetPlayer) return player.sendMessage(`§6[§eSafeGuard§6]§f Player §e${setName}§f was not found`);
-		if (targetPlayer.hasAdmin()) {
-            data.player.sendMessage(`§6[§eSafeGuard§6]§f Can't copy the inventory of §e${targetPlayer.name}§f, they're an admin.`);
-		    return;
-        }
+		
         sendMessageToAllAdmins(`§6[§eSafeGuard Notify§6]§e ${player.name} §fcopied the inventory of§e ${targetPlayer.name}§f! §r`,true);
 		copyInv(player,targetPlayer);
     }
