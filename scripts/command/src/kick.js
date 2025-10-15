a72b6a5ff671b3db27dfcf1c6fbe4590f63d4065
Nebrian Cent <nebriancent@gmail.com>
2025-10-14 19:52:29 -0700

    Update kick.js

--- scripts/command/src/kick.js
+++ scripts/command/src/kick.js
@@ -10,10 +10,7 @@ newCommand({
         const setName = args.slice(1).join(" ").replace(/["@]/g, "");
         const targetPlayer = getPlayerByName(setName);
         if (!targetPlayer) return player.sendMessage(`§6[§eSafeGuard§6]§f Player §e${setName}§f was not found`);
-        if (targetPlayer.hasAdmin()) {
-            data.player.sendMessage(`§6[§eSafeGuard§6]§f Can't kick §e${targetPlayer.name}§f, they're an admin.`);
-            return;
-        }
+
         sendMessageToAllAdmins(`§6[§eSafeGuard Notify§6]§e ${player.name} §fkicked the player§e ${targetPlayer.name}§f! §r`, true);
         targetPlayer.runCommand(`kick @s you were kicked by ${player.name}`);
     }
