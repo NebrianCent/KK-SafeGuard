b6ec7e09ed9581c0c28667523075ef30dfc74374
Nebrian Cent <nebriancent@gmail.com>
2025-10-14 19:50:27 -0700

    Update clearwarn.js

--- scripts/command/src/clearwarn.js
+++ scripts/command/src/clearwarn.js
@@ -4,6 +4,7 @@ import { getPlayerByName,sendMessageToAllAdmins } from '../../assets/util';
 newCommand({
     name: "clearwarn",
     description: "<player> Clears the player's warnings",
+    ownerOnly: true,
     run: (data) => {
         const { player, args } = data;
 
@@ -13,10 +14,6 @@ newCommand({
 
         if (!targetPlayer) return player.sendMessage(`§6[§eSafeGuard§6]§f Player §e${setName}§f was not found`);
 
-        if (targetPlayer.hasAdmin()) {
-            player.sendMessage(`§6[§eSafeGuard§6]§f Can't clear the warns of §e${targetPlayer.name}§f, they're an admin.`);
-            return;
-        }
         sendMessageToAllAdmins(`§6[§eSafeGuard Notify§6]§e ${player.name} §fcleared the warnings of the player§e ${targetPlayer.name}! §r`, true);
         player.sendMessage(`§6[§eSafeGuard§6]§f Successfully cleared warnings.`);
         targetPlayer.clearWarnings();
